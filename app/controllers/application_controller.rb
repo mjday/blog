971cc936db1168039a4747966bd9cccd20901548
Michael Day <mjd.poindexter@gmail.com>
2019-10-11 16:30:07 +0800

    finish static pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,3 +5,9 @@ class ApplicationController < ActionController::Base
     render html: "hello, world!"
   end
 end
+
+
+# What I need to do once the app is ready:
+
+# change application.html back to it's original name
+# fix the gem file with all updated gems, update one by one to see what breaks
